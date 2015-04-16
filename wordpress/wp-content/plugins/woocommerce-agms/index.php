30683d3ba8ea1b7aaebb37d591c02c6efd01984b
Maanas Royy <maanas@maanas.co>
2015-04-17 02:24:41 +0530

    Added license, readme

---
+++ wordpress/wp-content/plugins/woocommerce-agms/index.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Plugin Name: WooCommerce AGMS Payment Gateway
+Plugin URI: https://github.com/agmscode/woocommerce-agms
+Description: Agms Payment gateway for woocommerce
+Version: 0.1
+Author: Maanas Royy
+Author URI: https://github.com/maanas
+*/
